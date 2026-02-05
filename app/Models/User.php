a2e66be5de8135c0b1773a9a3205a16f725134dc
Hardik Shah <sunshine.mahant@gmail.com>
2026-02-06 00:33:24 +1100

    Add Laravel Sanctum integration
    Add Laravel Sanctum to the project to enable SPA and API token authentication. Changes include: add "laravel/sanctum" to composer.json and composer.lock; add config/sanctum.php; create migration for personal_access_tokens; add HasApiTokens trait to App\Models\User; register stateful API middleware in bootstrap/app.php; update various docs/manifests and rule files to mention Sanctum. Also adjust some seeder field types (datetime -> timestamp, integer -> int8) in database/seeders/specs/User.json.

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
 /**
@@ -33,7 +34,7 @@ final class User extends Authenticatable implements FilamentUser, MustVerifyEmai
     /**
      * @use HasFactory<UserFactory>
      */
-    use HasFactory, HasRoles, Notifiable, TwoFactorAuthenticatable;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * @var list<string>
